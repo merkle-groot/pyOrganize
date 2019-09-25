62bdb92c7fa522cce54590b61bec9fca3b61a69a
Vishnu Prakash <stanbrooks07@gmail.com>
2019-09-25 10:37:22 +0530

    git pull

--- pyOrganize.py
+++ pyOrganize.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import getpass
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
@@ -21,71 +22,41 @@ class eventHandler(PatternMatchingEventHandler):
                 os.rename(pathDownloads+".Temp/"+file,pathDownloads+"Documents/"+file)
 
             else:
-                os.rename(pathDownloads+".Temp/"+file,pathDownloads+"Others/"+file)    
-
-
-
-
-    
-        # if len(listF)!=0:
-        #     for file in listF:
-        #         if file.endswith((".jpg",".png",".gif",".exif",".tiff",".bmp",".svg")):
-        #             os.rename(pathDownloads+"Temp",pathDownloads+"Images/"+file)
-
+                os.rename(pathDownloads+".Temp/"+file,pathDownloads+"Others/"+file)   
 
 
     def process(self,event):
         print(event.src_path)
-        fileName=event.src_path.replace(pathDownloads,"")
-        os.rename(pathDownloads+fileName,pathDownloads+".Temp/"+fileName)
-        self.sort()
-
-        # if "*.png" in event.src_path:
-        #    fileName=event.src_path.replace(pathDownloads,"")
-        #    os.rename(pathDownloads+fileName,pathDownloads+"Images/"+fileName)
-
-        # elif "*.mp4" in event.src_path:
-        #    fileName=event.src_path.replace(pathDownloads,"")
-        #    os.rename(pathDownloads+fileName,pathDownloads+"Videos/"+fileName)
-
-        # elif "*.pdf" in event.src_path:
-        #    fileName=event.src_path.replace(pathDownloads,"")
-        #    os.rename(pathDownloads+fileName,pathDownloads+"Documents/"+fileName)   
-
-        # elif ".zip" in event.src_path:
-        #    fileName=event.src_path.replace(pathDownloads,"")
-        #    os.rename(pathDownloads+fileName,pathDownloads+"ZipFiles/"+fileName)
-
-
-
-
-        #print(event.src_path)
+        if not (event.src_path).endswith(".part"):
+            fileName=event.src_path.replace(pathDownloads,"")
+            os.rename(pathDownloads+fileName,pathDownloads+".Temp/"+fileName)
+            self.sort()
 
     def on_created(self,event):
         self.process(event)
 
 
+if __name__ == "__main__":
+    folders=["Videos","Images","Documents","Others"]
+    pathDownloads=os.path.expanduser("~")+"/Downloads/"
 
-folders=["Videos","Images","Documents","Others"]
-pathDownloads="/home/"+sys.argv[1]+"/Downloads/"
 
-
-if not os.path.exists(pathDownloads+"log.txt"):
-    with open(pathDownloads+".log.txt",'w') as file:
-        os.mkdir(pathDownloads+".Temp")
-        for i in range(len(folders)):
-            os.mkdir(pathDownloads+folders[i])
-        file.write("Initialised Folders")
+    if not os.path.exists(pathDownloads+".log.txt"):
+        with open(pathDownloads+".log.txt",'w') as file:
+            os.mkdir(pathDownloads+".Temp")
+            for i in range(len(folders)):
+                os.mkdir(pathDownloads+folders[i])
+            file.write("Initialised Folders")
 
     #args=sys.argv[1:]
-    
-observer=Observer()
-observer.schedule(eventHandler(),path=pathDownloads)        
-observer.start()
 
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    observer.stop()
-    observer.join()
+    observer=Observer()
+    observer.schedule(eventHandler(),path=pathDownloads)        
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+        observer.join()
